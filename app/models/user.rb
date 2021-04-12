6040ac11a197453153ef4c6aa21ea371be87caa7
Colin Schlecht <schlechtcolin@yahoo.com>
2021-04-12 11:39:25 -0700

    model relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+    has_many :collections
+    has_many :artists
+    has_many :arts
+    has_many :arts, through: :collections
+    has_many :comments, through: :arts
+
 end
