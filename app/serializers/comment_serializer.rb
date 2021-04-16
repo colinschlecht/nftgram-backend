17c10a2db58aa7954ef114ddd470d7353bb9dece
Colin Schlecht <schlechtcolin@yahoo.com>
2021-04-16 16:23:29 -0700

    pre - serializer swap

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,5 +1,7 @@
 class CommentSerializer
   include JSONAPI::Serializer
-  attributes 
+  belongs_to :user
+  belongs_to :art
+  attributes :user_id, :art_id, :comment
   set_key_transform :camel_lower
 end
