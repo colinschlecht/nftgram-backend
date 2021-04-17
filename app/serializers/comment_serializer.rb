dd18d802035102f038d1149d7ca4a38be8985b42
Colin Schlecht <schlechtcolin@yahoo.com>
2021-04-16 17:07:32 -0700

    updates comment serializer

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,5 +1,5 @@
 class CommentSerializer < ActiveModel::Serializer
   belongs_to :user
   belongs_to :art
-  attributes :id, :comments, :user_id, :art_id, :comment, :created_at
+  attributes :id, :user_id, :art_id, :comment, :created_at
 end
