de6b03881f3aa642665d870c03060e40ece33fb8
Colin Schlecht <schlechtcolin@yahoo.com>
2021-04-16 19:36:01 -0700

    adds likes to user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,6 +2,7 @@ class UserSerializer < ActiveModel::Serializer
   has_many :collections
   has_many :artists
   has_many :arts
-  has_many :comments, through: :arts
+  has_many :likes
+  has_many :comments
   attributes :id, :username, :password_digest, :bio, :link
 end
