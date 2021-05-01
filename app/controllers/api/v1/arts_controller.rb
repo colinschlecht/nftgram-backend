f93b3de391b69f7245fd64726892c163e7d89348
Colin Schlecht <schlechtcolin@yahoo.com>
2021-04-30 21:25:46 -0700

    removes byebug

--- app/controllers/api/v1/arts_controller.rb
+++ app/controllers/api/v1/arts_controller.rb
@@ -20,14 +20,11 @@ class Api::V1::ArtsController < ApplicationController
   def create
     categ = Category.find_or_create_by(name: art_params[:category].downcase)
     new_params = art_params.except("category", "artwork")
-    # byebug
+    
     art = Art.new(new_params)
     art.category_id = categ.id
-    byebug
     if art.save
-      byebug
-      art.artwork.attach(art_params[:artwork])
-      byebug
+      # art.artwork.attach(art_params[:artwork])
       render json: art
     else
       render json: { error: "failed to create art" }, status: 400
@@ -42,7 +39,7 @@ class Api::V1::ArtsController < ApplicationController
       :artist_id,
       :category,
       :for_sale,
-      :artwork,
+      # :artwork,
       # :likes,
       # :slug,
       # :description,
