578f1f81f1ea03193ddbee0fa9c08029207a8b43
Colin Schlecht <schlechtcolin@yahoo.com>
2021-06-18 18:19:41 -0700

    added user to artist serializer

--- app/serializers/artist_serializer.rb
+++ app/serializers/artist_serializer.rb
@@ -1,5 +1,5 @@
 class ArtistSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :name  
+  attributes :id, :user_id, :name, :user 
   belongs_to :user
   has_many :arts
   has_many :collections
