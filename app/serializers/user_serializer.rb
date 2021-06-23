8ae087db596146f4022a512102c8b0e8e3bfae2d
Colin Schlecht <schlechtcolin@yahoo.com>
2021-06-23 12:14:09 -0700

    adds user avatar to serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -4,5 +4,5 @@ class UserSerializer < ActiveModel::Serializer
   has_many :arts
   has_many :likes
   has_many :comments
-  attributes :id, :username, :metamask_account, :bio
+  attributes :id, :username, :metamask_account, :bio, :avatar
 end
