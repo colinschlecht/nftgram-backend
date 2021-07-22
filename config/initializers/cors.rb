c147822292ec638e19c5f2a2c1cbbdf2a271b4a5
Colin Schlecht <schlechtcolin@yahoo.com>
2021-07-21 19:46:26 -0700

    adds user and art edit to admin section

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://nftgram.io", "https://www.nftgram.io"
+    origins "https://nftgram.io", "https://www.nftgram.io", "localhost:3001"
     # origins "*"
 
     resource "*",
