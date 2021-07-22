de277441f8ae2bc7877a6771b41d50ecf2e605ad
Colin Schlecht <schlechtcolin@yahoo.com>
2021-07-21 19:44:48 -0700

    updates serializer for art and user for admin page

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,5 +2,5 @@ class UserSerializer < ActiveModel::Serializer
   has_many :arts
   has_many :likes
   has_many :comments
-  attributes :id, :username, :metamask_account, :bio, :avatar
+  attributes :id, :username, :artist_id, :metamask_account, :bio, :avatar, :created_at, :updated_at
 end
